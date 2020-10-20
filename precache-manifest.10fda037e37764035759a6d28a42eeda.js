562a8a71cd7a9e43f79dc523bc8e8c783a3658a7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-20 21:11:30 +0000

    Deploy thiagohissa/SPA_week05_comp3104_lab_exec to github.com/thiagohissa/SPA_week05_comp3104_lab_exec.git:gh-pages

---
+++ precache-manifest.10fda037e37764035759a6d28a42eeda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57d49223bbfbc1653929",
+    "url": "/static/css/main.6731251d.chunk.css"
+  },
+  {
+    "revision": "57d49223bbfbc1653929",
+    "url": "/static/js/main.57d49223.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "9a6744596d17db0806fed49ebcdcb2bd",
+    "url": "/index.html"
+  }
+];
